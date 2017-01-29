4887e1d43548e1b80057030c751f269d1cbc2665
HollyMadCow <zlwh2y@163.com>
2017-01-29 16:18:39 +0800

    more

--- src/cn/yhgaj/wjdd/search.java
+++ src/cn/yhgaj/wjdd/search.java
@@ -20,23 +20,57 @@ import javax.sql.DataSource;
 
 //import javax.servlet.http.HttpServletRequest;
 //import javax.servlet.http.HttpServletResponse;
-
+import org.json.*;
 
 public class search extends HttpServlet {
     //服务器端代码
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-//        response.setContentType("text/html;charset=UTF-8");
-        response.setContentType("application/x-json");
+        response.setContentType("text/html;charset=UTF-8");
+//        response.setContentType("application/x-json;charset=UTF-8");
         PrintWriter out = response.getWriter();
+        Connection conn = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+
+
         try {
             String searchkeyword = request.getParameter("keyword");
 //            String teststring = String.format("{'name':%s}",searchkeyword);
             if (searchkeyword == null) {
                 out.println("用户名不能为空");
             } else {
+                Context initCtx = new InitialContext();
+                Context ctx = (Context) initCtx.lookup("java:comp/env");
+//获取连接池对象
+                DataSource ds =(DataSource)ctx.lookup("jdbc/ConnectionPool");
+//创建连接
+                conn = ds.getConnection();
+                stmt = conn.createStatement();
+                String sql=String.format("select username,userid from USER WHERE userid='%s'","331021198209300019");
+                rs = stmt.executeQuery(sql);
+                JSONArray array = new JSONArray();
+                ResultSetMetaData rsmd = rs.getMetaData();
+                int columnCount = rsmd.getColumnCount();
 
-                out.println(searchkeyword);
+
+                while(rs.next())
+                {
+                    JSONObject jsonObj = new JSONObject();
+                    for(int i=1;i<columnCount+1;i++)
+                    {
+
+                        String columnName =rsmd.getColumnLabel(i);
+                        String value = rs.getString(columnName);
+                        jsonObj.put(columnName, value);
+
+                    }
+                    //System.out.println();
+                    array.put(jsonObj);
+                }
+
+                out.println(array);
+//                out.println(searchkeyword);
 //                response.getWriter().println(searchkeyword);
 //                RequestDispatcher de=request.getRequestDispatcher("/pki/search.jsp");
 //                de.forward(request, response);
@@ -51,7 +85,17 @@ public class search extends HttpServlet {
 //                //out.println("<a href=\"classic.html\">返回校验页面</a>");
             }
 
-        } finally {
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }finally {
+            try{
+                if(rs != null) rs.close();
+                if(stmt != null) stmt.close();
+                if(conn != null) conn.close();
+            }catch(Exception e1){
+                e1.printStackTrace();
+            }
             out.close();
         }
     }
--- web/index.jsp
+++ web/index.jsp
@@ -1,16 +1,56 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: HollyWin
-  Date: 2017/1/27
-  Time: 13:14
-  To change this template use File | Settings | File Templates.
---%>
+<!DOCTYPE html>
+<html lang="cn">
+<meta http-equiv="X-UA-Compatible" content="IE=9" />
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<html>
-  <head>
-    <title>$Title$</title>
-  </head>
-  <body>
-  $END$
-  </body>
+<head>
+  <title>玉环网警配侦系统</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery-3.1.1.min.js"></script>
+</head>
+<script>
+  function pkilogin() {
+      alert("test");
+  }
+</script>
+<style>
+  body{
+    margin-left:auto;
+    margin-right:auto;
+    margin-TOP:400PX;
+    width:23em;
+  }
+</style>
+<body>
+<!--下面是用户名输入框-->
+<div class="input-group">
+  <h1 class="text-center">
+    玉环网警配侦系统
+  </h1>
+</div>
+<br>
+<!--下面是用户名输入框-->
+<div class="input-group">
+  <span class="input-group-addon" id="basic-addon1">@</span>
+  <input id="userName" type="text" class="form-control" placeholder="用户名" aria-describedby="basic-addon1">
+</div>
+<br />
+<!--下面是密码输入框-->
+<div class="input-group">
+  <span class="input-group-addon" id="basic-addon2">@</span>
+  <input id="passWord" type="text" class="form-control" placeholder="密码" aria-describedby="basic-addon1">
+</div>
+<br>
+<!--下面是登陆按钮,包括颜色控制-->
+<div class="input-group">
+  <a href="http://172.16.210.251/nopki/main.jsp" style="width:280px" class="btn btn-default">登  录</a>
+</div>
+<br>
+<!--下面是登陆按钮,包括颜色控制-->
+<!--<button type="submit" style="width:280px;" class="btn btn-success">PKI 登 录</button>-->
+<%--<a href="https://192.168.2.114/servlet/pkilogin" style="width:280px" class="btn btn-success">PKI登录</a>--%>
+<a href="javascript:void(0)" id="pkilogin" onclick="pkilogin()" style="width:280px" class="btn btn-success">PKI登录</a>
+</body>
 </html>
